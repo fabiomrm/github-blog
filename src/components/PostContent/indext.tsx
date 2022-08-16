de0a71f5f17febb07227f5e34390c0da61c7370e
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-08-16 07:50:15 -0300

    GithubContext

--- src/components/PostContent/indext.tsx
+++ src/components/PostContent/indext.tsx
@@ -12,5 +12,6 @@ export function PostContent() {
         JavaScript is a loosely typed and dynamic language. Variables in JavaScript are not directly associated with any particular value type, and any variable can be assigned (and re-assigned) values of all types:
       </p>
     </PostContentContainer>
+
   )
 }
